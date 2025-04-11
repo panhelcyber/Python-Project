ef24d92582b428440ae4ef5bc2adcdc6f713909d
panhelcyber <pankaj1011199@gmail.com>
2025-04-11 21:49:16 +0530

    Second Project in python

---
+++ Qr_code_generator.py
@@ -0,0 +1,15 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data("https://github.com/panhelcyber/Python-Project")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="red", back_color="blue")
+img.show()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python-Project
-#This is a cafe named with a panhel 
+1. This is a cafe named with a panhel
+2. Qr code generator in python language
+
